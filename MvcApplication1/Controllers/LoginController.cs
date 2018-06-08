cf3e1c991bba7dd3412e74ab90f40e0b51fe6be0
gaoqiang1994 <524587958@qq.com>
2018-06-08 10:51:25 +0800

    添加MD5加密工具类

---
+++ MvcApplication1/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using MvcApplication1.Models;
+using MvcApplication1.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user) 
+        {
+
+            string pwd = SecurityHelper.str2md5(user.Password);
+
+            if (User!=null)
+            {
+                return RedirectToRoute("/home");
+                
+            }
+            return View();
+
+        }
+
+    }
+}
